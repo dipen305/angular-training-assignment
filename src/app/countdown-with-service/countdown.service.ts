3410b2c22b3a656aa21996d687cb2eddaa11f8d8
Dipen Hirani <hirdhanj@WKMZT098B24A.global.publicisgroupe.net>
2022-07-04 19:44:37 +0530

    Moved functions to service and css changes

--- src/app/countdown-with-service/countdown.service.ts
+++ src/app/countdown-with-service/countdown.service.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
@@ -5,12 +6,88 @@ import { BehaviorSubject, Observable, Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class CountdownService {
+  interval:any;
 startStopLog :any[] = [];
 pauseLog:any[]=[];
 startCount:number = 0;
 pauseCount:number = 0;
 timer = new Subject<any>;
 currentTimer:number = 0;
-timerStarted = new Subject<any>;
-constructor() { }
+timerStarted:boolean =false;
+errorMessage:string= ''
+constructor(private datePipe: DatePipe) { }
+
+timerHandler(timerLimit:any,currentTimer:any){
+  this.errorMessage = '';
+  if (
+    (timerLimit != null && timerLimit != undefined && timerLimit !=0) ||
+    (currentTimer !=null  && currentTimer != undefined && currentTimer !=0)
+  ) {
+    if (currentTimer == null || currentTimer == undefined || currentTimer == 0) {
+      this.currentTimer = timerLimit;
+      this.startTimer("");
+    } 
+    else if (timerLimit != null) {
+      currentTimer = timerLimit;
+      this.stopTimer("reset");
+      this.startStopLog = [];
+      this.pauseLog = [];
+      this.startCount = 0;
+      this.pauseCount = 0;
+      this.startTimer("reset");
+    } 
+    else if (currentTimer >= 0 && this.timerStarted) {
+      this.startTimer("");
+    } else if (currentTimer >= 0 && !this.timerStarted) {
+      this.stopTimer("");
+    }
+  }
+  else {
+    this.errorMessage = 'Please enter timer limit';
+  }
+}
+
+startTimer(message: any) {
+  if (message == "reset") {
+    this.startCount = 1;
+    this.pauseCount = 0;
+  } else {
+    this.startCount++;
+  }
+  let date = new Date();
+  this.startStopLog.push(
+    "Started at " + this.datePipe.transform(date, "dd-MM-yyyy hh:mm:ss a")
+  );
+  this.interval = setInterval(() => {
+    this.currentTimer--;
+    this.currentTimer = this.currentTimer;
+
+    if (this.currentTimer <= 0) {
+      this.stopTimer("finish");
+    }
+  }, 1000);
+}
+stopTimer(message: any){
+  let date = new Date();
+  let prefix = "Paused at ";
+
+  if (message != "finish") {
+    this.pauseCount++;
+    this.pauseLog.push(prefix + this.currentTimer);
+  }
+  if (message == "finish") prefix = "Finished at ";
+  this.startStopLog.push(
+    prefix + this.datePipe.transform(date, "dd-MM-yyyy hh:mm:ss a")
+  );
+  clearInterval(this.interval);
+}
+reset(){
+  this.stopTimer("reset");
+    this.currentTimer = 0;
+    this.startStopLog = [];
+    this.pauseLog = [];
+    this.startCount = 0;
+    this.pauseCount = 0;
+    this.errorMessage = '';
+}
 }
--- src/app/countdown-with-service/timer-controller/timer-controller-with-service.component.ts
+++ src/app/countdown-with-service/timer-controller/timer-controller-with-service.component.ts
@@ -14,96 +14,24 @@ import { CountdownService } from "../countdown.service";
   templateUrl: "./timer-controller-with-service.component.html",
   styleUrls: ["./timer-controller-with-service.component.scss"],
 })
-export class TimerControllerWithServiceComponent implements OnInit, OnDestroy {
+export class TimerControllerWithServiceComponent implements OnDestroy {
   timerLimit: any;
-  timerStarted: boolean = false;
-  interval: any;
-  currentTimer: any;
   constructor(
     public countDownService: CountdownService,
-    private datePipe: DatePipe
   ) {}
-  ngOnInit() {}
   startPauseClick() {
     if (this.timerLimit > 0) {
-      this.timerStarted = true;
+      this.countDownService.timerStarted = true;
     } else {
-      this.timerStarted = !this.timerStarted;
+      this.countDownService.timerStarted = !this.countDownService.timerStarted;
     }
-    this.startTimer();
-  }
-
-  startTimer() {
-    if (
-      (this.timerLimit != null && this.timerLimit != undefined) ||
-      (this.currentTimer != null && this.currentTimer != undefined)
-    ) {
-      if (this.currentTimer == null || this.currentTimer == undefined) {
-        this.countDownService.currentTimer = this.timerLimit;
-        this.currentTimer = this.timerLimit;
-        this.timerLimit = null;
-        this.startInterval("");
-      } else if (this.timerLimit != null) {
-        this.currentTimer = this.timerLimit;
-        this.countDownService.currentTimer = this.timerLimit;
-        this.timerLimit = null;
-        this.stopInterval("reset");
-        this.countDownService.startStopLog = [];
-        this.countDownService.pauseLog = [];
-        this.countDownService.startCount = 0;
-        this.countDownService.pauseCount = 0;
-        this.startInterval("reset");
-      } else if (this.currentTimer >= 0 && this.timerStarted) {
-        this.startInterval("");
-      } else if (this.currentTimer >= 0 && !this.timerStarted) {
-        this.stopInterval("");
-      }
-    }
-  }
-
-  startInterval(message: any) {
-    if (message == "reset") {
-      this.countDownService.startCount = 1;
-      this.countDownService.pauseCount = 0;
-    } else {
-      this.countDownService.startCount++;
-    }
-    let date = new Date();
-    this.countDownService.startStopLog.push(
-      "Started at " + this.datePipe.transform(date, "dd-MM-yyyy hh:mm:ss a")
-    );
-    this.interval = setInterval(() => {
-      this.currentTimer--;
-      this.countDownService.currentTimer = this.currentTimer;
-
-      if (this.currentTimer <= 0) {
-        this.stopInterval("finish");
-      }
-    }, 1000);
-  }
-  stopInterval(message: any) {
-    let date = new Date();
-    let prefix = "Paused at ";
-
-    if (message != "finish") {
-      this.countDownService.pauseCount++;
-      this.countDownService.pauseLog.push(prefix + this.currentTimer);
-    }
-    if (message == "finish") prefix = "Finished at ";
-    this.countDownService.startStopLog.push(
-      prefix + this.datePipe.transform(date, "dd-MM-yyyy hh:mm:ss a")
-    );
-    clearInterval(this.interval);
-  }
-  reset() {
-    this.stopInterval("reset");
-    this.countDownService.currentTimer = 0;
-    this.countDownService.startStopLog = [];
-    this.countDownService.pauseLog = [];
-    this.countDownService.startCount = 0;
-    this.countDownService.pauseCount = 0;
+    this.countDownService.timerHandler(this.timerLimit,this.countDownService.currentTimer);
+    this.timerLimit = null;
   }
   ngOnDestroy(): void {
-    this.reset();
+    this.countDownService.reset();
+  }
+  reset(){
+    this.countDownService.reset();
   }
 }
