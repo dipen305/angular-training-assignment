2e87a8205b3848cf7d1e7e72be8ec86150993915
Dipen Hirani <hdipen@gmail.com>
2022-07-06 15:44:00 +0530

    Fixed sonar identified issues

--- src/app/dynamic-box/dynamic-box.component.spec.ts
+++ src/app/dynamic-box/dynamic-box.component.spec.ts
@@ -1,4 +1,3 @@
-/* tslint:disable:no-unused-variable */
 import { waitForAsync, ComponentFixture, TestBed } from "@angular/core/testing";
 import { DynamicBoxComponent } from "./dynamic-box.component";
 
--- src/app/shared/services/countdown.service.ts
+++ src/app/shared/services/countdown.service.ts
@@ -1,93 +1,94 @@
-import { DatePipe } from '@angular/common';
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { DatePipe } from "@angular/common";
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CountdownService {
-  interval:any;
-startStopLog :any[] = [];
-pauseLog:any[]=[];
-startCount:number = 0;
-pauseCount:number = 0;
-timer = new Subject<any>;
-currentTimer:number = 0;
-timerStarted:boolean =false;
-errorMessage:string= ''
-constructor(private datePipe: DatePipe) { }
+  interval: any;
+  startStopLog: any[] = [];
+  pauseLog: any[] = [];
+  startCount: number = 0;
+  pauseCount: number = 0;
+  timer = new Subject();
+  currentTimer: number = 0;
+  timerStarted: boolean = false;
+  errorMessage: string = "";
+  constructor(private datePipe: DatePipe) { }
 
-timerHandler(timerLimit:any,currentTimer:any){
-  this.errorMessage = '';
-  if (
-    (timerLimit != null && timerLimit != undefined && timerLimit !=0) ||
-    (currentTimer !=null  && currentTimer != undefined && currentTimer !=0)
-  ) {
-    if (currentTimer == null || currentTimer == undefined || currentTimer == 0) {
-      this.currentTimer = timerLimit;
-      this.startTimer("");
-    } 
-    else if (timerLimit != null) {
-      currentTimer = timerLimit;
-      this.stopTimer("reset");
-      this.startStopLog = [];
-      this.pauseLog = [];
-      this.startCount = 0;
+  timerHandler(timerLimit: any, currentTimer: any) {
+    this.errorMessage = "";
+    if (
+      (timerLimit != null && timerLimit != undefined && timerLimit != 0) ||
+      (currentTimer != null && currentTimer != undefined && currentTimer != 0)
+    ) {
+      if (
+        currentTimer == null ||
+        currentTimer == undefined ||
+        currentTimer == 0
+      ) {
+        this.currentTimer = timerLimit;
+        this.startTimer("");
+      } else if (timerLimit != null) {
+        currentTimer = timerLimit;
+        this.stopTimer("reset");
+        this.startStopLog = [];
+        this.pauseLog = [];
+        this.startCount = 0;
+        this.pauseCount = 0;
+        this.startTimer("reset");
+      } else if (currentTimer >= 0 && this.timerStarted) {
+        this.startTimer("");
+      } else if (currentTimer >= 0 && !this.timerStarted) {
+        this.stopTimer("");
+      }
+    } else {
+      this.errorMessage = "Please enter timer limit";
+    }
+  }
+
+  startTimer(message: any) {
+    if (message == "reset") {
+      this.startCount = 1;
       this.pauseCount = 0;
-      this.startTimer("reset");
-    } 
-    else if (currentTimer >= 0 && this.timerStarted) {
-      this.startTimer("");
-    } else if (currentTimer >= 0 && !this.timerStarted) {
-      this.stopTimer("");
+    } else {
+      this.startCount++;
     }
-  }
-  else {
-    this.errorMessage = 'Please enter timer limit';
-  }
-}
+    let date = new Date();
+    this.startStopLog.push(
+      "Started at " + this.datePipe.transform(date, "dd-MM-yyyy hh:mm:ss a")
+    );
+    this.interval = setInterval(() => {
+      this.currentTimer--;
+      // this.currentTimer = this.currentTimer;
 
-startTimer(message: any) {
-  if (message == "reset") {
-    this.startCount = 1;
-    this.pauseCount = 0;
-  } else {
-    this.startCount++;
+      if (this.currentTimer <= 0) {
+        this.stopTimer("finish");
+      }
+    }, 1000);
   }
-  let date = new Date();
-  this.startStopLog.push(
-    "Started at " + this.datePipe.transform(date, "dd-MM-yyyy hh:mm:ss a")
-  );
-  this.interval = setInterval(() => {
-    this.currentTimer--;
-    this.currentTimer = this.currentTimer;
+  stopTimer(message: any) {
+    let date = new Date();
+    let prefix = "Paused at ";
 
-    if (this.currentTimer <= 0) {
-      this.stopTimer("finish");
+    if (message != "finish") {
+      this.pauseCount++;
+      this.pauseLog.push(prefix + this.currentTimer);
     }
-  }, 1000);
-}
-stopTimer(message: any){
-  let date = new Date();
-  let prefix = "Paused at ";
-
-  if (message != "finish") {
-    this.pauseCount++;
-    this.pauseLog.push(prefix + this.currentTimer);
+    if (message == "finish") prefix = "Finished at ";
+    this.startStopLog.push(
+      prefix + this.datePipe.transform(date, "dd-MM-yyyy hh:mm:ss a")
+    );
+    clearInterval(this.interval);
   }
-  if (message == "finish") prefix = "Finished at ";
-  this.startStopLog.push(
-    prefix + this.datePipe.transform(date, "dd-MM-yyyy hh:mm:ss a")
-  );
-  clearInterval(this.interval);
-}
-reset(){
-  this.stopTimer("reset");
+  reset() {
+    this.stopTimer("reset");
     this.currentTimer = 0;
     this.startStopLog = [];
     this.pauseLog = [];
     this.startCount = 0;
     this.pauseCount = 0;
-    this.errorMessage = '';
-}
+    this.errorMessage = "";
+  }
 }
